ff4e13a81d5ababb3c640b8f629b7a0be0e67747
Livia <liviasydriao@gmail.com>
2025-10-18 18:43:54 -0300

    ultimas atualizações da autenticação e renomear SecurityWebConfig

--- backend/library/src/main/java/br/edu/unichristus/security/config/SecurityWebConfig.java
+++ backend/library/src/main/java/br/edu/unichristus/security/config/SecurityWebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class SecurityWebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
