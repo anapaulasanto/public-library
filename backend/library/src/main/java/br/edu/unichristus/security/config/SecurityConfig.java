cf1eab9509fab2486d6c057510f9f15fe04ab323
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2025-11-11 11:05:12 -0300

    corrige autenticacao no swagger

--- backend/library/src/main/java/br/edu/unichristus/security/config/SecurityConfig.java
+++ backend/library/src/main/java/br/edu/unichristus/security/config/SecurityConfig.java
@@ -30,44 +30,30 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        //Permite todas as requisições OPTIONS (preflight)
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
-                        // !! CORREÇÃO AQUI !!
-                        // Rotas públicas (registro, login, logout)
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/*","/v3/api-docs/*",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
 
-                        // Rotas públicas (frontend)
-                        // Assumindo que estas não são rotas de API
                         .requestMatchers("/", "/catalog/books").permitAll()
 
-                        // Protege as rotas de Admin
-                        // Se /admin/** for rota de API, mude para /api/v1/admin/**
                         .requestMatchers("/admin/**").hasRole("ADMIN")
 
-                        // Protege as rotas de User
-                        // Se /user/** for rota de API, mude para /api/v1/user/**
                         .requestMatchers("/user/**").hasRole("USER")
 
-                        // Rotas de User — usuários e admins
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
 
-                        // Rotas de Review — apenas usuários
                         .requestMatchers("/api/v1/review/**").hasRole("USER")
 
-                        // Rotas de livros (CRUD) — apenas administradores
                         .requestMatchers("/api/v1/book/**").hasRole("ADMIN")
 
-                        // Rotas de livros da API externa — apenas usuários
                         .requestMatchers("/api/v1/book/external/**").hasRole("USER")
 
-                        // Rotas de Category — apenas administradores
                         .requestMatchers("/api/v1/category/**").hasRole("ADMIN")
 
-                        // Rotas de Rental — qualquer usuário autenticado
                         .requestMatchers("/api/v1/rental/**").authenticated()
 
-                        // Todas as outras rotas exigem autenticação
                         .anyRequest().authenticated()
                 )
                 // Stateless, sem sessão
--- frontend/src/components/catalog/Main/InputSearch/index.jsx
+++ frontend/src/components/catalog/Main/InputSearch/index.jsx
@@ -3,12 +3,12 @@ import { IoSearch } from "react-icons/io5";
 
 export const InputSearch = () => {
     return (
-        <div className="flex border border-gray-300 bg-neutral-200 py-5 rounded-xl w-[95%] mx-auto mt-10  items-center">
+        <div className="flex  py-5 rounded-xl w-[95%] mx-auto mt-10  items-center">
             <form className="w-[85%] mx-auto flex justify-center items-center">
                 <input
                     type="text"
                     placeholder="Buscar livros, autores ou palavra-chave.. "
-                    className="relative border border-gray-300 p-3 w-full pl-10 rounded-xl bg-neutral-50 text-gray-500 text-sm font-semibold focus:outline-none focus:ring-2 focus:ring-blue-300 lg:w-[50%]"
+                    className="relative border border-gray-300 p-3 w-full pl-10 rounded-xl bg-neutral-50 text-gray-600 text-sm font-semibold focus:outline-none focus:ring-2 focus:ring-blue-300 lg:w-[50%]"
                 />
                 <div className="w-[12%] p-3">
                     <select defaultValue="Livro" className="select rounded-xl border border-neutral-300 outline-none bg-neutral-100 shadow text-sm cursor-pointer">
@@ -17,7 +17,7 @@ export const InputSearch = () => {
                         <option>Palavra-chave</option>
                     </select>
                 </div>
-                <button type="submit" className="bg-blue-300 text-white p-3 ml-3 rounded-lg text-sm font-semibold hover:bg-blue-400 transition-all duration-200 cursor-pointer">
+                <button type="submit" className="bg-sky-700 text-white p-3 ml-3 rounded-full text-sm font-semibold hover:bg-sky-600 transition-all duration-200 cursor-pointer">
                     <IoSearch />
                 </button>
             </form>
