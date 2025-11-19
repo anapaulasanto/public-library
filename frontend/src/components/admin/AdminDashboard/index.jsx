3ec70172026874da2bfa081fa37b1b3ea638afe6
marilce <marilce@lesteti.com.br>
2025-11-19 18:28:18 -0300

    Corrige nome da pasta para bookContext.

--- frontend/src/components/admin/AdminDashboard/index.jsx
+++ frontend/src/components/admin/AdminDashboard/index.jsx
@@ -1,6 +1,6 @@
 import { MenuPages } from "../main/MenuPages"
 import { useContext } from "react";
-import { BookContext } from "../../../context/BookContext";
+import { BookContext } from "../../../context/bookContext";
 import { SectionTableBooks } from "../main/sectionAdmin/SectionTableBooks";
 import { SectionTableUsers } from "../main/sectionAdmin/SectionTableUsers";
 import { SectionTableCategory } from "../main/sectionAdmin/SectionTableCategory";
